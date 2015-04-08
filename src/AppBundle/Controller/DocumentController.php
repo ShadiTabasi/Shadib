0066422e6ddbdc2cf8a8c6b7b5eaca968177ec72
Hugues Maignol <hugues.maignol@kitpages.fr>
2015-04-08 14:31:43 +0200

    Edit a document (Controller action and view)

--- src/AppBundle/Controller/DocumentController.php
+++ src/AppBundle/Controller/DocumentController.php
@@ -10,6 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use \DateTime;
 
 
 class DocumentController extends Controller
@@ -63,6 +64,36 @@ class DocumentController extends Controller
         }
 
         return ['form' => $form->createView()];
+    }
 
+    /**
+     * Here we use the Symfony's ParameterConverter :
+     *  - We indicate by typehinting we want a document parameter of the class Document
+     *  - We named our route parameter {id}
+     *  - It understands what to do because we have a  mapped Doctrine entity for Document
+     *  - It calls "magically" the Doctrine->Manager->DocumentRepository->find($id) method
+     *  - It gives the result of this call to our $document parameter.
+     *
+     *  - If there is no result, it throws a 404 error for us
+     *
+     * @Route("/documents/{id}/edit")
+     * @Template()
+     */
+    public function editAction(Request $request, Document $document)
+    {
+
+        $form = $this->createForm('document', $document);
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $document->setUpdated(new DateTime());
+                $em->flush();
+            }
+
+            return $this->redirectToRoute('app_document_index');
+        }
+
+        return ['form' => $form->createView()];
     }
 }
