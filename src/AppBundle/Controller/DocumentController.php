a3db84b13bb76cc2e1f75a3be323d88177e05ad3
Hugues Maignol <hugues.maignol@kitpages.fr>
2015-04-08 11:46:24 +0200

    Document Index

---
+++ src/AppBundle/Controller/DocumentController.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Document;
+use AppBundle\Entity\Person;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+class DocumentController extends Controller
+{
+
+    /**
+     * @Route("/documents/")
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+        $personId = $request->get('person');
+        $em = $this->getDoctrine()->getManager();
+        if ($personId) {
+            $person = $em->getRepository('AppBundle:Person')->find($personId);
+            if (! $person) {
+                throw new NotFoundHttpException();
+            }
+        } else {
+            $person = null;
+        }
+        $documentRepository = $em->getRepository('AppBundle:Document');
+        if (! $personId) {
+            $documents = $documentRepository->findAll();
+        } else {
+            $documents = $documentRepository->findBy(['author' => $person]);
+        }
+
+        return ['documents' => $documents, 'person' => $person];
+    }
+}
---
+++ src/AppBundle/Controller/PersonController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Document;
+use AppBundle\Entity\Person;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class PersonController extends Controller
+{
+
+}
